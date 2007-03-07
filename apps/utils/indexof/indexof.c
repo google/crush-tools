15c480374c56d8be668327f37296a45d57727137
jhinds <jhinds@9f305d85-0c50-0410-b3e0-65f4174694de>
2007-03-07 18:49:39 +0000

    indexof: bug fix - failed to read the correct file line when a row number 	greater than one was specified.  also misc formatting changes.

--- apps/utils/indexof/indexof.c
+++ apps/utils/indexof/indexof.c
@@ -6,23 +6,27 @@
 
 int indexof ( struct cmdargs *args, int argc, char *argv[], int optind ){
 	FILE *in;
-	int lineno = 1, curline = 1, index = 1;
+	int lineno = 1, curline = 0, index = 1;
 	char *buf = NULL, *tok;
 	size_t buflen = 0;
 
-	if( args->file != NULL ){
-		if( (in = fopen(args->file, "r")) == NULL ){
+	if ( args->file != NULL ) {
+		if ( (in = fopen(args->file, "r")) == NULL ) {
 			perror(args->file);
 			return EXIT_FILE_ERR;
 		}
-	} else
+	}
+	else {
 		in = stdin;
+	}
+
 	if( args->row != 0 )
 		sscanf(args->row, "%d", &lineno);
 
-	if( args->string == NULL ){
+	if ( args->string == NULL ) {
+		/* read header from file */
 		do {
-			if( getline( &buf, &buflen, in ) == -1 ){
+			if ( getline( &buf, &buflen, in ) == -1 ) {
 				free(buf);
 				fprintf(stderr, "out of memory\n");
 				return EXIT_MEM_ERR;
@@ -30,13 +34,16 @@ int indexof ( struct cmdargs *args, int argc, char *argv[], int optind ){
 			curline++;
 		} while ( lineno > curline && ! feof(in) );
 
-		if( feof(in) ){
+		if ( feof(in) ) {
 			fprintf(stderr, "invalid line number: %d\n", lineno);
 			exit(EXIT_HELP);
 		}
-	} else {
+	}
+	else {
+		/* header string provided on commandline, but strtok needs
+		   a dynamically allocated copy */
 		buf = malloc(strlen(args->string) + 1);
-		if( buf == NULL ){
+		if ( buf == NULL ) {
 			fprintf(stderr, "out of memory\n");
 			return EXIT_MEM_ERR;
 		}
@@ -44,11 +51,11 @@ int indexof ( struct cmdargs *args, int argc, char *argv[], int optind ){
 	}
 
 	tok = strtok(buf, args->delim);
-	while( tok != NULL && strcmp( args->label, tok ) != 0 ){
+	while ( tok != NULL && strcmp( args->label, tok ) != 0 ) {
 		tok = strtok(NULL, args->delim);
 		index++;
 	}
-	if( tok == NULL )
+	if ( tok == NULL )
 		index = 0;
 
 	free(buf);
