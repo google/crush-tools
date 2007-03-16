71432cb7e92c5fbd3ca2d40d296e7f3c587dd4dd
jhinds <jhinds@9f305d85-0c50-0410-b3e0-65f4174694de>
2007-03-16 19:32:47 +0000

    ffutils: added another header for data-transfer-related defines, etc.

---
+++ src/c/ffutils/data_transfer.h
@@ -0,0 +1,32 @@
+
+#ifndef DATA_TRANSFER_H
+#define DATA_TRANSFER_H
+
+#define DT_HEADER_TIME              "Time"
+#define DT_HEADER_USER_ID           "User-ID"
+#define DT_HEADER_IP                "IP"
+#define DT_HEADER_ADVERTISER_ID     "Advertiser-ID"
+#define DT_HEADER_BUY_ID            "Buy-ID"
+#define DT_HEADER_AD_ID             "Ad-ID"
+#define DT_HEADER_CREATIVE_ID       "Creative-ID"
+#define DT_HEADER_SITE_ID           "Site-ID"
+#define DT_HEADER_PAGE_ID           "Page-ID"
+#define DT_HEADER_ACTIVITY_TYPE     "Activity-Type"
+#define DT_HEADER_ACTIVITY_SUB_TYPE "Activity-Sub-Type"
+#define DT_HEADER_QUANTITY          "Quantity"
+#define DT_HEADER_REVENUE           "Revenue"
+#define DT_HEADER_LOCAL_USER_ID     "Local-User-ID"
+#define DT_HEADER_TRANSACTION_ID    "Transaction-ID"
+#define DT_HEADER_OTHER_DATA        "Other-Data"
+#define DT_HEADER_ORDINAL           "Ordinal"
+#define DT_HEADER_CLICK_TIME        "Click-Time"
+#define DT_HEADER_EVENT_ID          "Event-ID"
+
+
+#define DT_EVENT_ID_UNMATCHED  0
+#define DT_EVENT_ID_POST_CLICK 1
+#define DT_EVENT_ID_POST_IMP   2
+
+
+#endif /* DATA_TRANSFER_H */
+
