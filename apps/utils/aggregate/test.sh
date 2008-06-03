6bd7db7614880139ae90ed59dfb92b72f036e387
jhinds <jhinds@9f305d85-0c50-0410-b3e0-65f4174694de>
2008-06-03 20:00:33 +0000

    apps/utils/aggregate/test.sh: be noisy about skipped tests; exit non-zero if   any tests failed.

--- apps/utils/aggregate/test.sh
+++ apps/utils/aggregate/test.sh
@@ -5,7 +5,7 @@ wdir=`dirname $0`
 datadir=$wdir/test
 infile=$datadir/test.in
 
-tests=("-p -k 1 -s 3" "-p -k 1,2 -s 3,4" "-p -k -a 3,4")
+tests=("-p -k 1 -s 3" "-p -k 1,2 -s 3,4" "-p -k 1 -s 3,4 -a 3,4")
 test_names=("one key, one sum (header preserved)" "multi-key, multi-sum (header preserved)" "one key, multi-average (header preserved)")
 errors=0
 
@@ -18,24 +18,30 @@ for i in `seq 0 $(( ${#tests[*]} - 1 ))`; do
 	
 	echo -n "test $i - \"${test_names[$i]}\": "
 
-	if [ -e $infile.$optstring.expected ]; then
+	if [ ! -e $infile.$optstring.expected ]; then
+		echo "skipped ($infile.$optstring.expected doesn't exist)"
+		continue
+	fi
 
-		actual=$infile.$optstring.actual
-		expected=$infile.$optstring.expected
+	actual=$infile.$optstring.actual
+	expected=$infile.$optstring.expected
 
-		${wdir}/aggregate ${tests[$i]} $infile \
-			> $actual
+	${wdir}/aggregate ${tests[$i]} $infile \
+		> $actual
 
-		if [ "`diff -q $actual $expected`" ]; then
-			(( errors++ ))
-			echo "failed"
-			echo "		see $actual"
-		else
-			echo "passed"
-			rm $actual
-		fi
+	if [ "`diff -q $actual $expected`" ]; then
+		(( errors++ ))
+		echo "failed"
+		echo "		see $actual"
+	else
+		echo "passed"
+		rm $actual
 	fi
 done
 
 echo -e "\n$errors errors\n"
 
+if [ $errors -eq 0 ]; then
+  exit 0
+fi
+exit 1
