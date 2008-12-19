1f9fcf5d29f2aa8502ead2211dcf5c8695c15d0b
jimrenwick <jimrenwick@9f305d85-0c50-0410-b3e0-65f4174694de>
2008-12-19 23:21:15 +0000

    src/translate_field: changes based on feedback to r400.
    roll all the individual xpression evals into a single eval and memoize
    work done on a field translation.
    
    * tests/test_04.sh: another test
    * translate_field.in: perf improvements.

--- src/translate_field/tests/test_04.sh
+++ src/translate_field/tests/test_04.sh
@@ -3,26 +3,22 @@ description="use expressions"
 
 infile="$test_dir/test.in"
 expected="$test_dir/test_$test_number.expected"
+subtest=0
 
-subtest=1
-outfile="$test_dir/test_$test_number.$subtest.actual"
-$bin -f 1 -x "s/CODE/hack/i" -x "s/a/123/" "$test_dir/test.in" > "$outfile"
-if [ $? -ne 0 ] ||
-   [ "`diff -q $outfile $expected`" ]; then
-  test_status $test_number $subtest "$description (index)" FAIL
-else
-  test_status $test_number $subtest "$description (index)" PASS
-  rm "$outfile"
-fi
+function ok {
+  local _label=$1; shift
+  ((subtest++))
+  outfile="$test_dir/test_$test_number.$subtest.actual"
+  eval $* "$test_dir/test.in" > "$outfile"
+  if [ $? -ne 0 ] ||
+    [ "`diff -q $outfile $expected`" ]; then
+    test_status $test_number $subtest "$description ($_label)" FAIL
+  else
+    test_status $test_number $subtest "$description ($_label)" PASS
+    rm "$outfile"
+  fi
+}
 
-subtest=2
-outfile="$test_dir/test_$test_number.$subtest.actual"
-$bin -F Field-0 -x "s/CODE/hack/i" -x "s/a/123/" "$test_dir/test.in" > "$outfile"
-if [ $? -ne 0 ] ||
-   [ "`diff -q $outfile $expected`" ]; then
-  test_status $test_number $subtest "$description (label)" FAIL
-else
-  test_status $test_number $subtest "$description (label)" PASS
-  rm "$outfile"
-fi
-
+ok index $bin -f 1 -x "s/CODE/hack/i" -x "s/a/123/"
+ok label $bin -F Field-0 -x "s/CODE/hack/i" -x "s/a/123/"
+ok index $bin -f 1 -x "s/CODE/hack/i" -x "s/a/123/" -x "s/1/XXX/" -x "s/X+/1/"
