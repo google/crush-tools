bc0c37a3e2c036a8ede3e14ed74986cbe5c6bc1e
jeremy.hinds <jeremy.hinds@9f305d85-0c50-0410-b3e0-65f4174694de>
2009-02-08 03:54:40 +0000

    src/libcrush/{queue,hashfuncs}.c: make linebreak style consistant.

--- src/libcrush/hashfuncs.c
+++ src/libcrush/hashfuncs.c
@@ -7,16 +7,16 @@
   *
   * @param str string to be hashed
   * @return hashed value of the string
-  */  
+  */
 unsigned int djb2(unsigned char *str) {
-  unsigned int hash = 5381;
-  int c;
-  while ((c = *str++))
-    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-  return hash;
-}
+  unsigned int hash = 5381;
+  int c;
+  while ((c = *str++))
+    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+  return hash;
+}
 
-
+
 /** this algorithm was created for sdbm (a public-domain reimplementation of
   * ndbm) database library. it was found to do well in scrambling bits, causing
   * better distribution of the keys and fewer splits. it also happens to be a
@@ -30,16 +30,16 @@ unsigned int djb2(unsigned char *str) {
   *
   * @param str string to be hashed
   * @return hashed value of the string
-  */ 
+  */
 unsigned int sdbm(unsigned char *str) {
-  unsigned int hash = 0;
-  int c;
-  while ((c = *str++))
-    hash = c + (hash << 6) + (hash << 16) - hash;
-  return hash;
-}
+  unsigned int hash = 0;
+  int c;
+  while ((c = *str++))
+    hash = c + (hash << 6) + (hash << 16) - hash;
+  return hash;
+}
 
-
+
 /* *********************************************************************
    from here down all from http://www.partow.net/programming/hashfunctions/
 	under common public license :
@@ -47,89 +47,89 @@ unsigned int sdbm(unsigned char *str) {
 
    minor modifications made by jhinds to make a common prototype for all
    hashes so they can be used interchangeably.
-   ********************************************************************* */ 
-  
+   ********************************************************************* */
+
 /** A simple hash function from Robert Sedgwicks Algorithms in C book. I've
   * added some simple optimizations to the algorithm in order to speed up its
   * hashing process.
   *
   * @param str string to be hashed
   * @return hashed value of the string
-  */ 
+  */
 unsigned int RSHash(unsigned char *str) {
-  unsigned int b = 378551;
-  unsigned int a = 63689;
-  unsigned int hash = 0;
-  int c;
-  while ((c = *str++)) {
-    hash = hash * a + c;
-    a = a * b;
-  }
-  return (hash & 0x7FFFFFFF);
-}
+  unsigned int b = 378551;
+  unsigned int a = 63689;
+  unsigned int hash = 0;
+  int c;
+  while ((c = *str++)) {
+    hash = hash * a + c;
+    a = a * b;
+  }
+  return (hash & 0x7FFFFFFF);
+}
 
-
+
 /** A bitwise hash function written by Justin Sobel.
   *
   * @param str string to be hashed
   * @return hashed value of the string
-  */ 
+  */
 unsigned int JSHash(unsigned char *str) {
-  unsigned int hash = 1315423911;
-  int c;
-  while ((c = *str++)) {
-    hash ^= ((hash << 5) + c + (hash >> 2));
-  }
-  return (hash & 0x7FFFFFFF);
-}
+  unsigned int hash = 1315423911;
+  int c;
+  while ((c = *str++)) {
+    hash ^= ((hash << 5) + c + (hash >> 2));
+  }
+  return (hash & 0x7FFFFFFF);
+}
 
-
+
 /** This hash algorithm is based on work by Peter J. Weinberger of AT&T Bell
   * Labs.
   *
   * @param str string to be hashed
   * @return hashed value of the string
-  */ 
+  */
 unsigned int PJWHash(unsigned char *str) {
-  unsigned int BitsInUnignedInt = (unsigned int) (sizeof(unsigned int) * 8);
-  unsigned int ThreeQuarters = (unsigned int) ((BitsInUnignedInt * 3) / 4);
-  unsigned int OneEighth = (unsigned int) (BitsInUnignedInt / 8);
-  unsigned int HighBits =
+  unsigned int BitsInUnignedInt = (unsigned int) (sizeof(unsigned int) * 8);
+  unsigned int ThreeQuarters = (unsigned int) ((BitsInUnignedInt * 3) / 4);
+  unsigned int OneEighth = (unsigned int) (BitsInUnignedInt / 8);
+  unsigned int HighBits =
     (unsigned int) (0xFFFFFFFF) << (BitsInUnignedInt - OneEighth);
-  unsigned int hash = 0;
-  unsigned int test = 0;
-  int c;
-  while ((c = *str++)) {
-    hash = (hash << OneEighth) + c;
-    if ((test = hash & HighBits) != 0) {
-      hash = ((hash ^ (test >> ThreeQuarters)) & (~HighBits));
-    }
-  }
-  return (hash & 0x7FFFFFFF);
-}
+  unsigned int hash = 0;
+  unsigned int test = 0;
+  int c;
+  while ((c = *str++)) {
+    hash = (hash << OneEighth) + c;
+    if ((test = hash & HighBits) != 0) {
+      hash = ((hash ^ (test >> ThreeQuarters)) & (~HighBits));
+    }
+  }
+  return (hash & 0x7FFFFFFF);
+}
 
-
+
 /** Similar to the PJW Hash function, but tweaked for 32-bit processors. Its
   * the hash function widely used on most UNIX systems.
   *
   * @param str string to be hashed
   * @return hashed value of the string
-  */ 
+  */
 unsigned int ELFHash(unsigned char *str) {
-  unsigned int hash = 0;
-  unsigned int x = 0;
-  int c;
-  while ((c = *str++)) {
-    hash = (hash << 4) + c;
-    if ((x = hash & 0xF0000000L) != 0) {
-      hash ^= (x >> 24);
-      hash &= ~x;
-    }
-  }
-  return (hash & 0x7FFFFFFF);
-}
+  unsigned int hash = 0;
+  unsigned int x = 0;
+  int c;
+  while ((c = *str++)) {
+    hash = (hash << 4) + c;
+    if ((x = hash & 0xF0000000L) != 0) {
+      hash ^= (x >> 24);
+      hash &= ~x;
+    }
+  }
+  return (hash & 0x7FFFFFFF);
+}
 
-
+
 /** This hash function comes from Brian Kernighan and Dennis Ritchie's book
   * "The C Programming Language". It is a simple hash function using a strange
   * set of possible seeds which all constitute a pattern of 31....31...31 etc,
@@ -148,7 +148,7 @@ and it could have been much better without sacrificing its "extreme simplicity."
 [see the second edition!] Many C programmers use this function without actually
 testing it, or checking something like Knuth's Sorting and Searching, so it
 stuck. It is now found mixed with otherwise respectable code, eg. cnews. sigh.
-[see also: tpop] 
+[see also: tpop]
 
 	unsigned long hash(unsigned char *str) {
 		unsigned int hash = 0;
@@ -163,18 +163,18 @@ stuck. It is now found mixed with otherwise respectable code, eg. cnews. sigh.
   *
   * @param str string to be hashed
   * @return hashed value of the string
-  */ 
+  */
 unsigned int BKDRHash(char *str) {
-  unsigned int seed = 131;    /* 31 131 1313 13131 131313 etc.. */
-  unsigned int hash = 0;
-  int c;
-  while ((c = *str++)) {
-    hash = (hash * seed) + c;
-  }
-  return (hash & 0x7FFFFFFF);
-}
+  unsigned int seed = 131;    /* 31 131 1313 13131 131313 etc.. */
+  unsigned int hash = 0;
+  int c;
+  while ((c = *str++)) {
+    hash = (hash * seed) + c;
+  }
+  return (hash & 0x7FFFFFFF);
+}
 
-
+
 /** This is the algorithm of choice which is used in the open source SDBM
   * project. The hash function seems to have a good over-all distribution for
   * many different data sets. It seems to work well in situations where there
@@ -182,17 +182,17 @@ unsigned int BKDRHash(char *str) {
   *
   * @param str string to be hashed
   * @return hashed value of the string
-  */ 
+  */
 unsigned int SDBMHash(unsigned char *str) {
-  unsigned int hash = 0;
-  int c;
-  while ((c = *str++)) {
-    hash = c + (hash << 6) + (hash << 16) - hash;
-  }
-  return (hash & 0x7FFFFFFF);
-}
+  unsigned int hash = 0;
+  int c;
+  while ((c = *str++)) {
+    hash = c + (hash << 6) + (hash << 16) - hash;
+  }
+  return (hash & 0x7FFFFFFF);
+}
 
-
+
 /** An algorithm produced by me [sic] Arash Partow. I took ideas from all of the
   * above hash functions making a hybrid rotative and additive hash function
   * algorithm based around four primes 3,5,7 and 11. There isn't any real
@@ -207,16 +207,16 @@ unsigned int SDBMHash(unsigned char *str) {
   *
   * @param str string to be hashed
   * @return hashed value of the string
-  */ 
+  */
 unsigned int APHash(unsigned char *str) {
-  unsigned int hash = 0;
-  unsigned int i = 0;
-  int c;
-  while ((c = *str++)) {
-    hash ^= ((i++ & 1) == 0) ? 
-      ((hash << 7) ^ c ^ (hash >> 3)) : (~((hash << 11) ^ c ^ (hash >> 5)));
-  }
-  return (hash & 0x7FFFFFFF);
-}
+  unsigned int hash = 0;
+  unsigned int i = 0;
+  int c;
+  while ((c = *str++)) {
+    hash ^= ((i++ & 1) == 0) ?
+      ((hash << 7) ^ c ^ (hash >> 3)) : (~((hash << 11) ^ c ^ (hash >> 5)));
+  }
+  return (hash & 0x7FFFFFFF);
+}
 
 
