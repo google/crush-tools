b0841416c24d1a9e968ff16dd2db1aabe9b4b9f1
warlock.cc <warlock.cc@9f305d85-0c50-0410-b3e0-65f4174694de>
2010-02-18 12:39:48 +0000

    src/fieldsplit: Revert r480 since it breaks compatibility.
    $ patch -R < <(svn diff -c 480)

--- src/fieldsplit/fieldsplit.c
+++ src/fieldsplit/fieldsplit.c
@@ -144,10 +144,8 @@ int fieldsplit (struct cmdargs *args, int argc, char *argv[], int optind) {
     field_index = atoi(args->field) - 1;
   }
   if (args->buckets) {
-    char blen[32];
     buckets = atoi(args->buckets);
-    snprintf(blen, sizeof(blen), "%d", buckets-1);
-    bucket_len = strlen(blen);
+    bucket_len = strlen(args->buckets);
   }
 
   field = xmalloc(128);
