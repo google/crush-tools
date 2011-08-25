7f67364cacd6cf0b90cfba60a7d3cdef2c955822
jeremy.hinds@gmail.com <jeremy.hinds@gmail.com@9f305d85-0c50-0410-b3e0-65f4174694de>
2011-08-25 16:38:16 +0000

    src/aggregate: avoid locale-specific errors due to differences in LC_NUMERIC conventions.
    src/aggregate/test/test_05.sh: test 3 different locales (if available). Add
      different expected outputs for each.

--- src/aggregate/test/test_05.sh
+++ src/aggregate/test/test_05.sh
@@ -8,26 +8,37 @@ subtest_opts=(
 '-l "Sum-1	Sum-2	Count-1	Count-2	Average-1	Average-2"'
 )
 
+LANG_BAK="$LANG"
+LC_NUMERIC_BAK="$LC_NUMERIC"
 for subtest in `seq 0 2`; do
-  expected="$test_dir/test_$test_number.$subtest.expected"
-  outfile="$test_dir/test_$test_number.$subtest.actual"
+  for locale in C en_US.utf8 es_AR.utf8; do
+    if [ ! "$(locale -a | grep $locale)" ]; then
+      continue
+    fi
+    export LANG=$locale
+    export LC_NUMERIC=$locale
+    expected="$test_dir/test_$test_number.$subtest.$locale.expected"
+    outfile="$test_dir/test_$test_number.$subtest.$locale.actual"
 
-  eval $bin -p \
-       -K Text-1,Text-2 \
-       -S Numeric-1,Numeric-2 \
-       -C Numeric-1,Numeric-2 \
-       -A Numeric-1,Numeric-2 \
-       "${subtest_opts[$subtest]}" \
-       "$test_dir/test.in" \
-       > "$outfile"
+    eval $bin -p \
+         -K Text-1,Text-2 \
+         -S Numeric-1,Numeric-2 \
+         -C Numeric-1,Numeric-2 \
+         -A Numeric-1,Numeric-2 \
+         "${subtest_opts[$subtest]}" \
+         "$test_dir/test.in" \
+         > "$outfile"
 
-  if [ $? -ne 0 ] ||
-     [ "`diff -q $outfile $expected`" ]; then
-    test_status $test_number $subtest \
-                "$description (${subtest_desc[$subtest]})" FAIL
-  else
-    test_status $test_number $subtest \
-                "$description (${subtest_desc[$subtest]})" PASS
-    rm "$outfile"
-  fi
+    if [ $? -ne 0 ] ||
+       [ "`diff -q $outfile $expected`" ]; then
+      test_status $test_number $subtest \
+                  "$description (${subtest_desc[$subtest]} $locale)" FAIL
+    else
+      test_status $test_number $subtest \
+                  "$description (${subtest_desc[$subtest]} $locale)" PASS
+      rm "$outfile"
+    fi
+  done
 done
+LANG="$LANG_BAK"
+LC_NUMERIC="$LC_NUMERIC_BAK"
